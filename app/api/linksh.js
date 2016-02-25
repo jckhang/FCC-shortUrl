2f9048ac405161f728b83599c44db367a822b5e9
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-02-25 12:24:13 -0500

    update

--- app/api/linksh.js
+++ app/api/linksh.js
@@ -5,7 +5,7 @@ module.exports = function(app, db) {
     //     // Check and retrieve url to redirect if it exist.
     //     .get(handleGet);
 
-    
+
     app.get('/api/linksh', function(req, res) {
         var name = req.query.name;
         var url = req.query.url;
@@ -17,46 +17,46 @@ module.exports = function(app, db) {
         console.log(name);
         console.log(url);
         res.send(urlObj)
-            // res.send(urlObj);
-            // save(urlObj, db);
+
+        save(urlObj, db);
     });
 
-    // function handleGet(req, res) {
-    //     var url = process.env.APP_URL + req.params.url;
-    //     if (url != process.env.APP_URL + 'favicon.ico') {
-    //         findURL(url, db, res);
-    //     }
-    // }
+    function handleGet(req, res) {
+        var url = process.env.APP_URL + req.params.url;
+        if (url != process.env.APP_URL + 'favicon.ico') {
+            findURL(url, db, res);
+        }
+    }
 
 
-    // function save(obj, db) {
-    //     // Save object into db.
-    //     var sites = db.collection('sites');
-    //     sites.save(obj, function(err, result) {
-    //         if (err) throw err;
-    //         console.log('Saved ' + result);
-    //     });
-    // }
+    function save(obj, db) {
+        // Save object into db.
+        var sites = db.collection('sites');
+        sites.save(obj, function(err, result) {
+            if (err) throw err;
+            console.log('Saved ' + result);
+        });
+    }
 
-    // function findURL(link, db, res) {
-    //     // Check to see if the site is already there
-    //     var sites = db.collection('sites');
-    //     // get the url
-    //     sites.findOne({
-    //         "short_url": link
-    //     }, function(err, result) {
-    //         if (err) throw err;
-    //         // object of the url
-    //         if (result) {
-    //             // we have a result
-    //             console.log('Found ' + result);
-    //             console.log('Redirecting to: ' + result.original_url);
-    //             res.redirect(result.original_url);
-    //         } else {
-    //             // we don't
-    //             res.send('Site not found');
-    //         }
-    //     });
-    // }
+    function findURL(link, db, res) {
+        // Check to see if the site is already there
+        var sites = db.collection('sites');
+        // get the url
+        sites.findOne({
+            "short_url": link
+        }, function(err, result) {
+            if (err) throw err;
+            // object of the url
+            if (result) {
+                // we have a result
+                console.log('Found ' + result);
+                console.log('Redirecting to: ' + result.original_url);
+                res.redirect(result.original_url);
+            } else {
+                // we don't
+                res.send('Site not found');
+            }
+        });
+    }
 
 };
